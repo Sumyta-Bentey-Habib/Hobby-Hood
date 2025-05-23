399833fde6c418f3405503e9fb67b532b995a796
Your Name <you@example.com>
2025-05-24 03:16:17 +0600

    updated

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -46,7 +46,7 @@ const CreateGroup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/hobbies", { // <-- changed here
+      const response = await fetch("http://localhost:3000/hobbies", { 
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
